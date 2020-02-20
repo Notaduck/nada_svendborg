c491d1ae8c656417d85c298503f5f6406c7347d1
Daniel Guldberg Aaes <guldberg72@gmail.com>
2020-02-20 23:05:26 +0100

    footer component has been added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/footer.css'
+
+const Footer: React.FC = () => {
+    return (
+        <div className="footer">
+            <h1>footer</h1>
+        </div>
+    )
+}
+
+export default Footer
