5ad61beae86ea45cadcf06cabf4a09618faf71f0
Daniel Guldberg Aaes <guldberg72@gmail.com>
2020-03-06 11:45:59 +0100

    the context of the sections has been updated

--- src/components/sections/TreatmentSection.js
+++ src/components/sections/TreatmentSection.js
@@ -8,18 +8,19 @@ const ThreatmentSection = ({ id, dark }) => (
     <div className='container'>
       <p>
         Tilbuddet er åbent 2 gange om ugen. Åbningstid er mandag og torsdag kl. 16-18, således man kan få sat NADA-nåle i kl. 16-17:15 og efterfølgende sidde med nålene i 45 min.
-      <br />
         <br />
-        Der er lukket på helligdage, mellem jul og nytår samt i juli måned.
-      <br />
+        <br />Der er lukket på helligdage, mellem jul og nytår samt i juli måned.
         <br />
-        Der kræves ikke tilmelding. Der gives behandling i den rækkefølge man møder op. Man skriver sig på en liste og får et nummer, som skrives på en oversigt, så det er tydeligt, hvornår nålene skal tages ud på den enkelte.
-      <br />
+        <br />Der kræves ikke tilmelding. Der gives behandling i den rækkefølge man møder op. Man registreres med fornavn på en liste og får et nummer. Behandleren kalder ind når tiden er gået, hvorefter nålene tages ud.
         <br />
-        Der er i alt ca. 15 NADA-behandlere i foreningen, så man vil møde forskellige behandlere fra gang til gang. Der er altid 2 behandlere til stede, når tilbuddet er åbent.
-      <br />
+        <br />Der er i alt ca. 20 NADA-behandlere i foreningen, så man vil møde forskellige behandlere fra gang til gang. Der er altid 2 behandlere til stede, når tilbuddet er åbent.
         <br />
-        Pris er 25 kr. pr. gang, som dækker udgifter til NADA-nåle m.v. Der betales kontant eller med MobilePay.
+        <br />Man sidder, eller ligger på yoga-måtter med et tæppe over, i et fælles lokale. Der er kun få polstrede stole. Vi har et vist antal yoga-måtter og tæpper til udlån.
+        <br />
+        <br />Prisen for behandlingen dækker udgifter til NADA-nåle m.v. Der betales kontant eller med MobilePay.
+        <br />
+        <br />
+        <br />
       </p>
       <img className='threatment_img' src={require('../../assets/threatment2.jpg')} alt='' />
     </div>
