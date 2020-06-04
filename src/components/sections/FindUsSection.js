2fa03e3e58598400cec161c0473f212679e42a90
Daniel Guldberg Aaes <guldberg72@gmail.com>
2020-06-04 20:40:08 +0200

    .

--- src/components/sections/FindUsSection.js
+++ src/components/sections/FindUsSection.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import Section from '../Section';
 import '../../styles/findus.css'
-import Map from '../Map/Map'
+
+
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
 const FindUsSection = ({ id, dark }) => (
   <Section id={id} dark={dark} cl='find' >
-    <div className='container'>
+    <div className="leaflet_container">
+      <Map
+        center={[50, 10]}
+        zoom={6}
+        maxZoom={10}
+        attributionControl={true}
+        zoomControl={true}
+        doubleClickZoom={true}
+        scrollWheelZoom={true}
+        dragging={true}
+        animate={true}
+        easeLinearity={0.35}
+      >
+
+      </Map>
+
+    </div>
+    {/* <div className='container'>
       <div className="contact_info">
         <p>
           NADA Svendborg
@@ -17,8 +36,7 @@ const FindUsSection = ({ id, dark }) => (
           mail: nadasvendborg@gmail.com
             </p>
       </div>
-      <Map />
-    </div>
+    </div> */}
   </Section>
 );
 
