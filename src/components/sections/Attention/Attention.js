d12303010148e8ca41048dc762d246adb6f2d902
Daniel Guldberg Aaes <guldberg72@gmail.com>
2020-10-15 10:55:03 +0200

    covid

--- src/components/sections/Attention/Attention.js
+++ src/components/sections/Attention/Attention.js
@@ -3,18 +3,14 @@ import Section from "../Section";
 
 const Attention = ({ id, dark }) => (
   <Section id={id} dark={dark}>
-	<h1>
-	Vi må desværre meddele, at NADA-cafeen stadig holder lukket på ubestemt tid pga Corona situationen.
-	<br/>
+	<p style={{ fontSize: "2rem" }}>
+	Vi må desværre meddele, at NADA-cafeen stadig holder lukket på ubestemt tid pga. Corona situationen.
 		Vi ved at vi er savnet, og ville så gerne have haft mulighed for at starte op.
-
-	<br/>
-	<br/>
 		Hvis du har behov for NADA henviser vi i stedet til private NADA-udøvere.
+	Du kan finde en liste over de registrerede NADA-udøvere på <a style={{ color: "inherit" }} href="https://nada-danmark.dk/?page_id=435">NADA-Danmarks hjemmeside</a>.
+	
 
-	<br/>
-		Du kan finde en liste over de registrerede NADA-udøvere her på NADA-Danmarks hjemmeside.
-	</h1>
+	</p>
   </Section>
 );
 
