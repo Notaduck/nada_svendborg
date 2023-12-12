437688a446fcd2da2531d320f28a5f0f68b52606
Daniel Guldberg Aaes <dev@guldberglab.info>
2023-12-12 13:42:53 +0100

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => (
     <div className="site_title">
       <h1 className="title">NADA Svendborg </h1>
       <h4 className="sub_title"> – certificerede nadaudøvere</h4>
-          <h4 className="sub_title"> Da frivillighuset flytter matrikel, holder NADA-cafeen lukket fra og med 15/1 - 31/1. Herefter starter vi op i de fantastiske nye lokaler i Frivillighuset Kvægtorvet, på Frederiksgade nr 8 , Svendborg (tidligere den ene del af FremtidsFabrikken)	For at fejre dette giver vi i februar måned GRATIS NADA til alle.      </h2>
+          <h4 className="sub_title"> Da frivillighuset flytter matrikel, holder NADA-cafeen lukket fra og med 15/1 - 31/1. Herefter starter vi op i de fantastiske nye lokaler i Frivillighuset Kvægtorvet, på Frederiksgade nr 8 , Svendborg (tidligere den ene del af FremtidsFabrikken)	For at fejre dette giver vi i februar måned GRATIS NADA til alle.   
  </h4>
     </div>
   </div>
